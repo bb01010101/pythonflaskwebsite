aea34d9123f9064a857802077d43036dc2bb37e7
bb01010101 <bb7906@princeton.edu>
2024-12-18 00:14:32 -0500

    attempt 2 adaptable.io

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ app = create_app()
 migrate = Migrate(app, db)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    # Get the PORT from the environment variable, default to 5000 if not set
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
 
     
