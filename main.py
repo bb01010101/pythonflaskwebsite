c550e2d387cde7c26382ebd282d3071a06e16345
bb01010101 <bb7906@princeton.edu>
2024-12-18 01:09:33 -0500

    revert main.py

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ app = create_app()
 migrate = Migrate(app, db)
 
 if __name__ == '__main__':
-    # Get the PORT from the environment variable, default to 5000 if not set
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run( debug=True)
 
 
     
