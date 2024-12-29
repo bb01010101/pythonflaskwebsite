29b10b186f7f7492c3f6f78af53aaf5fd5b9dc5c
bb01010101 <bb7906@princeton.edu>
2024-12-29 18:22:17 -0500

    fix!?

---
+++ migrations/versions/a9491326a1d6_match_current_db_state.py
@@ -0,0 +1,24 @@
+"""match_current_db_state
+
+Revision ID: a9491326a1d6
+Revises: 4117bcd49e82
+Create Date: 2024-12-29 18:21:56.875024
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a9491326a1d6'
+down_revision = '4117bcd49e82'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
