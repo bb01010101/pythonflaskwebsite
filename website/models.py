5496859bf0f43307e13f90128b21cf7494190c68
bb01010101 <bb7906@princeton.edu>
2025-01-15 15:48:25 -0500

    yee

--- website/models.py
+++ website/models.py
@@ -2,7 +2,7 @@ from . import db
 from flask_login import UserMixin 
 from sqlalchemy.sql import func
 import datetime
-from datetime import datetime, timezone
+from datetime import datetime, date, timezone
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
